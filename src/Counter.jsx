f5f1a8ba33b6b01362fdd2e9db7d9ba3b1c8ed06
Angel <90640417+AngelD99@users.noreply.github.com>
2021-09-29 11:48:20 -0500

    Update Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,15 +5,15 @@ import './Counter.css';
 
 export const Counter = () => {
     const [contador,setContador] = useState(0)
-    
+    //este metodo resta 1
     const handleDis = () => {
         setContador(contador - 1);
     }
-
+    //este metodo suma 1
     const handleAdd = () => {
         setContador(contador + 1);
     }
-
+    //este metodo resetea el valor del numero
     const handleRes = () => {
         setContador(0);
     }
